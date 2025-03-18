1d509445f5d351f216b3de198139df8e5fd36da2
Nishant Banakar <banakarnishant@gmail.com>
2025-03-18 22:23:06 +0530

    changed hello.cpp (Again)

--- main/hello.cpp
+++ main/hello.cpp
@@ -7,5 +7,6 @@ int main(void) {
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
   cout << "I used more than one brain cell for this assignment\n";
+  cout << "Seems like i forgot to add webhook before pushing :p\n";
   return 0;
 }
